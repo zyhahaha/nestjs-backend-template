f867c46fee75c3ab874abf1dda52a66476bde7c6
zhaoyang <zhaoyang@lyky.cn>
2024-10-11 15:17:41 +0800

    update

---
+++ src/utils/log4js_elk.ts
@@ -0,0 +1,92 @@
+import * as Path from 'path';
+import * as StackTrace from 'stacktrace-js';
+
+import winston from 'winston';
+import { ElasticsearchTransport } from 'winston-elasticsearch';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new ElasticsearchTransport({
+      level: 'info',
+      clientOpts: {
+        node: 'http://43.143.122.98:9222',
+        // auth: {
+        //   username: '用户名',
+        //   password: '密码'
+        // }
+      },
+      index: 'nestjs',  
+    }),
+  ],
+});
+
+logger.info('This is an info message');
+logger.error('This is an error message');
+
+// 日志级别
+export enum LoggerLevel {
+  ALL = 'ALL',
+  MARK = 'MARK',
+  TRACE = 'TRACE',
+  DEBUG = 'DEBUG',
+  INFO = 'INFO',
+  WARN = 'WARN',
+  ERROR = 'ERROR',
+  FATAL = 'FATAL',
+  OFF = 'OFF',
+}
+
+// 内容跟踪类
+export class ContextTrace {
+  constructor(
+    public readonly context: string,
+    public readonly path?: string,
+    public readonly lineNumber?: number,
+    public readonly columnNumber?: number,
+  ) { }
+}
+
+export class Logger {
+  static debug(...args) {
+    logger.debug(Logger.getStackTrace(), ...args);
+  }
+
+  static log(...args) {
+    logger.info(Logger.getStackTrace(), ...args);
+  }
+
+  static info(...args) {
+    logger.info(Logger.getStackTrace(), ...args);
+  }
+
+  static warn(...args) {
+    logger.warn(Logger.getStackTrace(), ...args);
+  }
+
+  static warning(...args) {
+    logger.warn(Logger.getStackTrace(), ...args);
+  }
+
+  static error(...args) {
+    logger.error(Logger.getStackTrace(), ...args);
+  }
+
+  static access(...args) {
+    // const loggerCustom = Log4js.getLogger('http');
+    // loggerCustom.info(Logger.getStackTrace(), ...args);
+  }
+
+  // 日志追踪，可以追溯到哪个文件、第几行第几列
+  static getStackTrace(deep = 2): string {
+    const stackList: StackTrace.StackFrame[] = StackTrace.getSync();
+    const stackInfo: StackTrace.StackFrame = stackList[deep];
+
+    const lineNumber: number = stackInfo.lineNumber;
+    const columnNumber: number = stackInfo.columnNumber;
+    const fileName: string = stackInfo.fileName;
+    const basename: string = Path.basename(fileName);
+    return `${basename}(line: ${lineNumber}, column: ${columnNumber}): \n`;
+  }
+}
