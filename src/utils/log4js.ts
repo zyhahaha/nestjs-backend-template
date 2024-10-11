83785f36ef1675bdffc1bec9e79ae3b8c82b7b57
zhaoyang <zhaoyang@lyky.cn>
2024-10-11 15:33:43 +0800

    update

--- src/utils/log4js.ts
+++ src/utils/log4js.ts
@@ -1,10 +1,26 @@
 import * as Path from 'path';
-import * as Log4js from 'log4js';
-import * as Util from 'util';
-import * as Moment from 'moment'; // 处理时间的工具
 import * as StackTrace from 'stacktrace-js';
-import Chalk from 'chalk';
-import config from 'src/config/log4js';
+
+const winston = require('winston');
+const { ElasticsearchTransport } = require('winston-elasticsearch');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new ElasticsearchTransport({
+      level: 'info',
+      clientOpts: {
+        node: 'http://43.143.122.98:9222',
+        // auth: {
+        //   username: '用户名',
+        //   password: '密码'
+        // }
+      },
+      index: 'nestjs',  
+    }),
+  ],
+});
 
 // 日志级别
 export enum LoggerLevel {
@@ -26,85 +42,10 @@ export class ContextTrace {
     public readonly path?: string,
     public readonly lineNumber?: number,
     public readonly columnNumber?: number,
-  ) {}
+  ) { }
 }
 
-Log4js.addLayout('Awesome-nest', (logConfig: any) => {
-  return (logEvent: Log4js.LoggingEvent): string => {
-    let moduleName = '';
-    let position = '';
-
-    // 日志组装
-    const messageList: string[] = [];
-    logEvent.data.forEach((value: any) => {
-      if (value instanceof ContextTrace) {
-        moduleName = value.context;
-        // 显示触发日志的坐标（行，列）
-        if (value.lineNumber && value.columnNumber) {
-          position = `${value.lineNumber}, ${value.columnNumber}`;
-        }
-        return;
-      }
-
-      if (typeof value !== 'string') {
-        value = Util.inspect(value, false, 3, true);
-      }
-
-      messageList.push(value);
-    });
-
-    // 日志组成部分
-    const messageOutput: string = messageList.join(' ');
-    const positionOutput: string = position ? ` [${position}]` : '';
-    const typeOutput = `[${logConfig.type}] ${logEvent.pid.toString()}   - `;
-    const dateOutput = `${Moment(logEvent.startTime).format(
-      'YYYY-MM-DD HH:mm:ss',
-    )}`;
-    const moduleOutput: string = moduleName
-      ? `[${moduleName}] `
-      : '[LoggerService] ';
-    let levelOutput = `[${logEvent.level}] ${messageOutput}`;
-
-    // 根据日志级别，用不同颜色区分
-    switch (logEvent.level.toString()) {
-      case LoggerLevel.DEBUG:
-        levelOutput = Chalk.green(levelOutput);
-        break;
-      case LoggerLevel.INFO:
-        levelOutput = Chalk.cyan(levelOutput);
-        break;
-      case LoggerLevel.WARN:
-        levelOutput = Chalk.yellow(levelOutput);
-        break;
-      case LoggerLevel.ERROR:
-        levelOutput = Chalk.red(levelOutput);
-        break;
-      case LoggerLevel.FATAL:
-        levelOutput = Chalk.hex('#DD4C35')(levelOutput);
-        break;
-      default:
-        levelOutput = Chalk.grey(levelOutput);
-        break;
-    }
-
-    return `${Chalk.green(typeOutput)}${dateOutput}  ${Chalk.yellow(
-      moduleOutput,
-    )}${levelOutput}${positionOutput}`;
-  };
-});
-
-// 注入配置
-Log4js.configure(config);
-
-// 实例化
-const logger = Log4js.getLogger();
-logger.level = LoggerLevel.TRACE;
-
 export class Logger {
-  static trace(...args) {
-    logger.trace(Logger.getStackTrace(), ...args);
-  }
-
   static debug(...args) {
     logger.debug(Logger.getStackTrace(), ...args);
   }
@@ -129,13 +70,9 @@ export class Logger {
     logger.error(Logger.getStackTrace(), ...args);
   }
 
-  static fatal(...args) {
-    logger.fatal(Logger.getStackTrace(), ...args);
-  }
-
   static access(...args) {
-    const loggerCustom = Log4js.getLogger('http');
-    loggerCustom.info(Logger.getStackTrace(), ...args);
+    // const loggerCustom = Log4js.getLogger('http');
+    // loggerCustom.info(Logger.getStackTrace(), ...args);
   }
 
   // 日志追踪，可以追溯到哪个文件、第几行第几列
